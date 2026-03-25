ff42c4092bda753cd78559a76ed37b812f5c93e9
limon <shahariarhasanlimon@gmail.com>
2026-03-26 00:12:15 +0600

    added loop2

---
+++ Module 3 Loop/loop_2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    
+    //for (i = 1; i <= 5; i+1) 
+    //for (i = 1; i <= 20; i+2) 
+    for(i = 10; i >= 1; i-1)             
+
+    {
+        printf("%d \n", i);
+    }
+    return 0;
+}
