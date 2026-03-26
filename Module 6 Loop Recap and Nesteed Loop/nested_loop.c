b3d3007a171dec452ae1bfb698c81f0952e824ff
limon <shahariarhasanlimon@gmail.com>
2026-03-26 09:44:42 +0600

    added nested loop

---
+++ Module 6 Loop Recap and Nesteed Loop/nested_loop.c
@@ -0,0 +1,13 @@
+//Nested For Loop Implementation
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
