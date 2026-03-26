143ff1b313575593208996ce685cb4d8b36a04f4
limon <shahariarhasanlimon@gmail.com>
2026-03-26 11:07:08 +0600

    addedprinting reverse array

--- Module 7 Introduction to Array/array_reverse_print.c
+++ Module 7 Introduction to Array/array_reverse_print.c
@@ -1,4 +1,4 @@
-//Array Input and Output
+//Printing Reverse of an Array
 #include<stdio.h>
 int main() {
     int arr[5]; //Declaration of an array of size 5
@@ -6,10 +6,10 @@ int main() {
     printf("Enter 5 integers:\n");
     for (int i = 0; i < 5; i++) {
         scanf("%d", &arr[i]);
-    }       
-    //Accessing and printing the elements of the array
-    printf("You entered:\n");
-    for (int i = 0; i < 5; i++) {
+    }
+    //Accessing and printing the elements of the array in reverse order
+    printf("The elements of the array in reverse order are:\n");    
+    for (int i = 4; i >= 0; i--) {
         printf("%d ", arr[i]);
     }
     return 0;
