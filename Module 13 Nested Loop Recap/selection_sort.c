5adbf638cbd9e608c4e6418d377a566c4508d63c
limon <shahariarhasanlimon@gmail.com>
2026-04-01 02:03:26 +0600

    added selection sort

---
+++ Module 13 Nested Loop Recap/selection_sort.c
@@ -0,0 +1,25 @@
+//selection sort 
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < n - 1; i++) {
+        int min_index = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_index]) {
+                min_index = j;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = arr[min_index];
+        arr[min_index] = temp;
+    }
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
