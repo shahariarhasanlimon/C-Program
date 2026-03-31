1af434d5d30562e200f2556a82e898e9d2dca681
limon <shahariarhasanlimon@gmail.com>
2026-04-01 00:00:25 +0600

    updated pattern1

--- Module 13 Nested Loop Recap/pattern_1.c
+++ Module 13 Nested Loop Recap/pattern_1.c
@@ -1,14 +1,18 @@
-//Pattern 
-#include<stdio.h>
+// Pattern
+#include <stdio.h>
 
-int main(){
-    int n,k;
-    scanf("%d",&n);
-    for(int i=1; i<=n; i++){
+int main()
+{
+    int n, k=1;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
 
-        for(int j=1;j<=k;j++){
+        for (int j = 1; j <= k; j++)
+        {
             printf("*");
         }
+        k++;
         printf("\n");
     }
     return 0;
